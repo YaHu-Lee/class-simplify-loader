0cf009264caaddf50311d89175393cde6263fa89
YaHu-Lee <lyh990719@gmail.com>
2021-04-20 22:34:01 +0800

    js-loader mvp

--- class-simplify-css-loader/index.js
+++ class-simplify-css-loader/index.js
@@ -1,4 +1,28 @@
+const postcss = require("postcss")
 module.exports = function loader(source) {
+  if(!global.domTreeArray) {
+    console.log("")
+  }
+  console.log("css loaded")
   console.log(source)
+  console.log(global.domTreeArray[0].children)
   return source
+  function findDomTree() {
+    const domTreeArray = global.domTreeArray
+    if(!domTreeArray){
+      // 警示处理
+    } else {
+      for(let index = 0; index < domTreeArray.length; index++) {
+        if(domTreeArray[index].matchedStyleNumber < domTreeArray[index].styleSheetNumber) {
+          // 这个domTree的样式表还没找全
+          // 这意味着 当前的 source 就是这个domTree的样式其中之一
+          domTreeArray[index].matchedStyleNumber++
+          simplifyCSS(domTreeArray[index].children[0])
+        }
+      }
+    }
+  }
+  function simplifyCSS(root) {
+    // root
+  }
 }
